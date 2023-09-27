f79ccbe3c9fe10fbf54b2c0783c975d43d68ac0f
jas Nagra <jaskarannagra@hotmail.com>
2023-09-26 21:54:18 -0700

    rough outline of 3 tables showing information. drive bc data fetched using there API

---
+++ sio_db_concept/src/app/components/RegionalDistrictTable.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import styles from "../styles/Region.module.css";
+
+export default function RegionalDistrictTable() {
+  const testData = [
+    {
+      name: "Northern Rockies Regional District",
+      Date: "Sept 25 2023",
+      URL: "https://www.northernrockies.ca/",
+    },
+    {
+      name: "Squamish Lillooet District",
+      Date: "Sept 23 2023",
+      URL: "https://www.slrd.bc.ca/",
+    },
+  ];
+
+  return (
+    <table>
+      <tr>
+        <th>Regional District</th>
+        <th>Most Recent Update</th>
+        <th>URL</th>
+      </tr>
+      {testData.map((region, index) => (
+        <tr className={styles.regionrow} key={index}>
+          <td>{region.name}</td>
+          <td>{region.Date}</td>
+          <td>
+            <a href={region.URL}>Link</a>
+          </td>
+        </tr>
+      ))}
+    </table>
+  );
+}
---
+++ sio_db_concept/src/app/components/WildfireTable.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+
+export default function WildfireTable() {
+  const testData = [
+    { name: "tatuk Lake", hectares: "30,000", status: "out of control" },
+    { name: "beaver Lake", hectares: "30,000", status: "out of control" },
+    {
+      name: "McDouggal Creek",
+      hectares: "30,000",
+      status: "being held",
+    },
+  ];
+
+  // Function to map status values to CSS classes
+  const getStatusClass = (status) => {
+    switch (status.toLowerCase()) {
+      case "being held":
+        return "row-bg-held";
+      case "out of control":
+        return "row-bg-out-of-control";
+      default:
+        return ""; // No specific class for other statuses
+    }
+  };
+
+  return (
+    <table>
+      <tr>
+        <th>Wildfire Name</th>
+        <th>Hectares</th>
+        <th>Current Status</th>
+      </tr>
+      {testData.map((fire, index) => (
+        <tr key={index} className={getStatusClass(fire.status)}>
+          <td>{fire.name}</td>
+          <td>{fire.hectares}</td>
+          <td>{fire.status}</td>
+        </tr>
+      ))}
+    </table>
+  );
+}
