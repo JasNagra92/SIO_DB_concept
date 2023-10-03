7fbfd219ccbdcdc62b7b2bdb8efd53d7862bf809
jasmac <jaskarannagra@hotmail.com>
2023-10-03 15:02:37 -0700

    fixed typo

--- sio_db_concept/src/pages/contactsPage.jsx
+++ sio_db_concept/src/pages/contactsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "@/components/layout";
-import ContactsPage from "@/Components/contacts";
+import ContactsPage from "@/components/contacts";
 
 export default function Contacts() {
   return (
